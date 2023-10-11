a8be9e2700310a6f4ef8327690fe570f816f0237
zxcv5595 <zxcv5595@korea.ac.kr>
2023-10-11 18:32:17 +0900

    프로젝트 시작

---
+++ src/main/java/com/checkyou/CheckyouApplication.java
@@ -0,0 +1,13 @@
+package com.checkyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/checkyou/CheckyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
