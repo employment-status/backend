84218d19c918ee30e30c5e11c4a8846c58777d1f
zxcv5595 <zxcv5595@korea.ac.kr>
2023-10-15 19:20:19 +0900

    feat/custom_error: 커스텀 예외 처리
    - CustomException 생성
    - exception handling 정의

---
+++ src/main/java/com/checkyou/global/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.checkyou.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final ErrorCode errorCode;
+    private final int status;
+    private final String message;
+
+}
---
+++ src/main/java/com/checkyou/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.checkyou.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ErrorResponse customExceptionHandler(CustomException e) {
+        log.error("'{}':'{}'", e.getErrorCode(), e.getErrorCode().getMessage());
+        return new ErrorResponse(e.getErrorCode(), e.getStatus(), e.getMessage());
+    }
+
+}
