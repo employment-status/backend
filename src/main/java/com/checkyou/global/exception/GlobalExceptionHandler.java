9ea18e7283847666f83e38bb38180864423135bc
zxcv5595 <zxcv5595@korea.ac.kr>
2023-10-16 23:13:28 +0900

    코드정렬

--- src/main/java/com/checkyou/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/checkyou/global/exception/GlobalExceptionHandler.java
@@ -18,36 +18,45 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> customExceptionHandler(CustomException e) {
         log.error("'{}':'{}'", e.getErrorCode(), e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getStatus())
-            .body(new ErrorResponse(e.getErrorCode(), e.getStatus(), e.getMessage()));
+                .body(new ErrorResponse(e.getErrorCode(), e.getStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(
-        MethodArgumentNotValidException ex) {
+            MethodArgumentNotValidException ex) {
         List<String> errors =
-            ex.getBindingResult().getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
+                ex.getBindingResult().getAllErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .toList();
 
-        return ResponseEntity.status(ex.getStatusCode().value()).body(new ErrorResponse(
-            ErrorCode.VALIDATION_FAILED,
-            ErrorCode.VALIDATION_FAILED.getStatus().value(),
-            errors.toString()));
+        return ResponseEntity.status(ex.getStatusCode().value())
+                .body(
+                        new ErrorResponse(
+                                ErrorCode.VALIDATION_FAILED,
+                                ErrorCode.VALIDATION_FAILED.getStatus().value(),
+                                errors.toString()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        return ResponseEntity.status(400).body(new ErrorResponse(
-            ErrorCode.ENUM_VALIDATION, ErrorCode.ENUM_VALIDATION.getStatus().value(),
-            ex.getMessage()));
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(400)
+                .body(
+                        new ErrorResponse(
+                                ErrorCode.ENUM_VALIDATION,
+                                ErrorCode.ENUM_VALIDATION.getStatus().value(),
+                                ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleInternalServerException(Exception ex) {
         log.error("Unhandled exception: {}", ex.getMessage(), ex);
 
-        return ResponseEntity.status(500).body(new ErrorResponse(
-            ErrorCode.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            ex.getMessage()));
+        return ResponseEntity.status(500)
+                .body(
+                        new ErrorResponse(
+                                ErrorCode.INTERNAL_SERVER_ERROR,
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                ex.getMessage()));
     }
 }
